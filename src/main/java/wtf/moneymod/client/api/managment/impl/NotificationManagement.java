600cf63cd17b480d52f2deddde11d6328284de23
sleepy <43454376+CPacketPlayerAction@users.noreply.github.com>
2021-11-02 21:47:56 +0300

    первый коммит в moneymod+4

--- src/main/java/wtf/moneymod/client/api/managment/impl/NotificationManagement.java
+++ src/main/java/wtf/moneymod/client/api/managment/impl/NotificationManagement.java
@@ -1,13 +1,17 @@
 package wtf.moneymod.client.api.managment.impl;
 
 import wtf.moneymod.client.api.managment.IManager;
+import wtf.moneymod.client.impl.notifications.Notification;
+import wtf.moneymod.client.impl.utility.Globals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yoursleep
  * @since 02 November 2021
  */
-public class NotificationManagement implements IManager<NotificationManagement> {
-
+public class NotificationManagement implements IManager<NotificationManagement>, Globals {
     @Override public NotificationManagement register() {
         return this;
     }
