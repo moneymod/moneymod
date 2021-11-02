34ceec8d5acecaf293c60f2597110cf9e42b9411
pighax <vardvefiron@gmail.com>
2021-11-03 00:47:50 +0300

    totem

--- src/main/java/wtf/moneymod/client/impl/module/combat/AutoTotem.java
+++ src/main/java/wtf/moneymod/client/impl/module/combat/AutoTotem.java
@@ -4,6 +4,9 @@ import club.cafedevelopment.reflectionsettings.annotation.Clamp;
 import club.cafedevelopment.reflectionsettings.annotation.Setting;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import org.lwjgl.input.Mouse;
 import wtf.moneymod.client.impl.module.Module;
 import wtf.moneymod.client.impl.utility.impl.player.ItemUtil;
 
@@ -13,7 +16,7 @@ public class AutoTotem extends Module {
     //pig pig pig
 
     @Setting(id = "Health", clamp = @Clamp(min = 1, max = 36)) int health = 16;
-    @Setting(id = "Mode") Mode mode = Mode.TOTEM;
+    @Setting(id = "Mode") Mode mode = Mode.CRYSTAL;
     @Setting(id = "RightClickGapple") boolean rightClickGapple = true;
 
     @Override
@@ -21,16 +24,20 @@ public class AutoTotem extends Module {
         if (nullCheck() || mc.currentScreen instanceof GuiInventory) return;
         float hp = mc.player.getHealth() + mc.player.getAbsorptionAmount();
         if (hp > health){
-            switch (mode){
-                case TOTEM:
-                    ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING));
-                    break;
-                case GAPPLE:
-                    ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.GOLDEN_APPLE));
-                    break;
-                case CRYSTAL:
-                    ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.END_CRYSTAL));
-                    break;
+            if (rightClickGapple && mc.player.getHeldItemMainhand().getItem() instanceof ItemSword && mc.gameSettings.keyBindUseItem.isKeyDown()){
+                ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.GOLDEN_APPLE));
+            } else {
+                switch (mode) {
+                    case TOTEM:
+                        ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING));
+                        break;
+                    case GAPPLE:
+                        ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.GOLDEN_APPLE));
+                        break;
+                    case CRYSTAL:
+                        ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.END_CRYSTAL));
+                        break;
+                }
             }
         } else {
             ItemUtil.swapItemsOffhand(ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING));
