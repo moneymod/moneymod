fc0b9002acd07d74646752f69c620b0d95bd23a8
cattyngmd <cvenby@gmail.com>
2021-11-08 17:34:42 +0300

    removed loader

--- src/main/java/wtf/moneymod/client/api/management/impl/ModuleManagement.java
+++ src/main/java/wtf/moneymod/client/api/management/impl/ModuleManagement.java
@@ -1,13 +1,14 @@
 package wtf.moneymod.client.api.management.impl;
 
+import net.minecraft.launchwrapper.Launch;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.reflections.Reflections;
 import wtf.moneymod.client.api.management.IManager;
 import wtf.moneymod.client.api.setting.Option;
 import wtf.moneymod.client.impl.module.Module;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -30,6 +31,16 @@ public class ModuleManagement extends ArrayList<Module> implements IManager<Modu
         });
         sort(Comparator.comparing(Module::getLabel));
         return this;
+    }
+
+    public boolean isShitExist() {
+        System.out.println("хуй");
+        try {
+            Class.forName("me.sleepy.loader.MoneymodEntryPoint");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public ArrayList<Module> get(Predicate<Module> predicate) {
