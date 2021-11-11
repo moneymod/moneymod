eedabc9535007c4b9885ede2a29e6410ce1f6640
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-11-12 00:30:04 +0300

    Update SpeedMine.java

--- src/main/java/wtf/moneymod/client/impl/module/player/SpeedMine.java
+++ src/main/java/wtf/moneymod/client/impl/module/player/SpeedMine.java
@@ -55,9 +55,10 @@ public class SpeedMine extends Module {
 
     @Override public void onTick() {
         //this swap code is sooo trash
-        if (!instant){
-            if (currentPos != null)
-            if (mc.world.getBlockState(currentPos).getBlock() instanceof BlockAir) currentPos = null;
+        if (!instant) {
+            if (currentPos != null) {
+                if (mc.world.getBlockState(currentPos).getBlock() instanceof BlockAir) currentPos = null;
+            }
         }
         if (swap) {
             mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, currentPos, EnumFacing.DOWN));
