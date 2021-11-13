ae74e7a0097918f42af4a1c83229bf6b1d7c202e
cattyngmd <cvenby@gmail.com>
2021-11-13 22:12:18 +0300

    ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй ваймок иди нахуй

--- src/main/java/wtf/moneymod/client/impl/module/global/ClickGui.java
+++ src/main/java/wtf/moneymod/client/impl/module/global/ClickGui.java
@@ -7,7 +7,7 @@ import wtf.moneymod.client.impl.module.Module;
 import wtf.moneymod.client.impl.ui.click.theme.Themes;
 import wtf.moneymod.client.impl.utility.impl.render.JColor;
 
-@Module.Register( label = "ClickGui", cat = Module.Category.GLOBAL, key = Keyboard.KEY_RETURN )
+@Module.Register( label = "ClickGui", cat = Module.Category.GLOBAL, key = Keyboard.KEY_INSERT )
 public class ClickGui extends Module {
 
     @Value( value = "Theme" ) public Themes theme = Themes.NODUS;
