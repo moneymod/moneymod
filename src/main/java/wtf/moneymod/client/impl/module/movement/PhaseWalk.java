40ccf447c004cfc3b84c2faf499bd0f37587e579
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-11-14 13:18:52 +0300

    Update PhaseWalk.java

--- src/main/java/wtf/moneymod/client/impl/module/movement/PhaseWalk.java
+++ src/main/java/wtf/moneymod/client/impl/module/movement/PhaseWalk.java
@@ -18,7 +18,12 @@ public class PhaseWalk extends Module {
 
     @Value(value = "Attempts") @Bounds(min = 1, max = 5) public int attempts = 1;
     @Value(value = "Speed") @Bounds(min = 1, max = 5) public int speed = 1;
-    @Value(value = "CheckGround") public boolean checkGround = true;
+    @Value(value = "Skip") public boolean skiper = false;
+    @Value(value = "SkipMode") public Skip skip = Skip.NPC1;
+
+    public enum Skip{
+        NPC1, NCP2, NCP3;
+    }
 
     Timer timer = new Timer();
     boolean cancel = false;
@@ -33,36 +38,43 @@ public class PhaseWalk extends Module {
         }
     });
 
+
+    @Handler
+    public Listener<MoveEvent> onMove = new Listener<>(MoveEvent.class, e -> {
+        if (nullCheck()) return;
+
+//        mc.player.motionX = 0.0;
+//        mc.player.motionY = 0.0;
+//        mc.player.motionZ = 0.0;
+        double[] dArray = EntityUtil.forward(speed / 150.0);
+        for (int i = 0; i < attempts; ++i) {
+            sendPackets(mc.player.posX + dArray[0], mc.player.posY, mc.player.posZ + dArray[1]);
+        }
+        if (mc.player.collidedHorizontally) {
+            e.motionX = 0.0; e.motionY = 0.0; e.motionZ = 0.0;
+        }
+    });
+
     @Override
     public void onTick() {
         if (nullCheck()) return;
         mc.player.motionX = 0.0; mc.player.motionY = 0.0; mc.player.motionZ = 0.0;
         if (mc.player.collidedHorizontally || mc.player.collidedVertically) {
-            if (timer.isPassed()) {
+            double y = mc.player.rotationYaw * 0.017453292;
+            double p = 0.01;
+            for (int i = 0; i < attempts; ++i) {
+                if (mc.gameSettings.keyBindSprint.isKeyDown()) {
+                    mc.player.movementInput.moveForward = 0.1f;
+                    sendPackets(mc.player.posX - Math.sin(y) * p, mc.player.posY + (mc.gameSettings.keyBindSneak.isKeyDown() ? -1 : 0) * speed / 100.0, mc.player.posZ + Math.cos(y) * p);
+                    mc.player.setPosition(mc.player.posX - Math.sin(y) * p, mc.player.posY + (mc.gameSettings.keyBindSneak.isKeyDown() ? -1 : 0) * speed / 100.0, mc.player.posZ + Math.cos(y) * p);
 
-                double[] dArray = EntityUtil.forward(speed / 100.0);
-
-                for (int i = 0; i < attempts; ++i)
-                    sendPackets(mc.player.posX + dArray[0], mc.player.posY + (mc.gameSettings.keyBindSneak.isKeyDown() ? -1 : 0) * speed / 100.0, mc.player.posZ + dArray[1]);
-
-                timer.reset();
-
+                }
             }
-        } else cancel = false;
-    }
-
-    public void sendPackets(double q, double w, double r) {
-        cancel = false;
-        mc.getConnection().sendPacket(new CPacketPlayer.Position(q, w, r, checkGround));
-        mc.getConnection().sendPacket(new CPacketPlayer.Position(0.0, 666, 0.0, checkGround));
-        cancel = true;
-    }
-
-    @Handler
-    public Listener<MoveEvent> onMove = new Listener<>(MoveEvent.class, e -> {
-        if (nullCheck()) return;
-        if (mc.player.collidedHorizontally) {
-            e.motionX = 0.0; e.motionY = 0.0; e.motionZ = 0.0;
         }
-    });
+    }
+
+    public void sendPackets(double x, double y, double z) {
+        mc.getConnection().sendPacket(new CPacketPlayer.Position(x, y, z, mc.player.onGround));
+        mc.getConnection().sendPacket(new CPacketPlayer.Position(0.0, 666, 0.0, mc.player.onGround));
+    }
 }
