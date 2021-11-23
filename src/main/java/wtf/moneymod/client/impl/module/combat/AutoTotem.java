141529de2f65aab2097d991837254f3acd0dbe6f
cattyngmd <cvenby@gmail.com>
2021-11-23 14:24:21 +0300

    нормальный хороший оффхен

--- src/main/java/wtf/moneymod/client/impl/module/combat/AutoTotem.java
+++ src/main/java/wtf/moneymod/client/impl/module/combat/AutoTotem.java
@@ -3,6 +3,7 @@ package wtf.moneymod.client.impl.module.combat;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemSword;
 import org.lwjgl.input.Mouse;
 import wtf.moneymod.client.api.setting.annotatable.Bounds;
@@ -15,35 +16,43 @@ public class AutoTotem extends Module {
 
     //pig pig pig
 
-    @Value(value = "Health") @Bounds(max = 36) public int health = 16;
-    @Value(value = "Item") public Mode mode = Mode.CRYSTAL;
-    @Value(value = "Right Click Gapple") public boolean rightClickGapple = true;
-
-    boolean doStrict;
-
+    @Value( value = "Health" ) @Bounds( max = 36 ) public int health = 16;
+    @Value( value = "Item" ) public Mode mode = Mode.CRYSTAL;
+    @Value( "Crapple" ) public boolean crapple = false;
+    @Value( value = "Right Click Gapple" ) public boolean rightClickGapple = true;
 
     @Override
     public void onTick() {
         if (nullCheck() || mc.currentScreen instanceof GuiInventory) return;
         float hp = mc.player.getHealth() + mc.player.getAbsorptionAmount();
-
-        if (rightClickGapple && mc.player.getHeldItemMainhand().getItem() instanceof ItemSword && mc.gameSettings.keyBindUseItem.isKeyDown()) {
-            ItemUtil.swapToOffhandSlot(ItemUtil.getItemSlot(Items.GOLDEN_APPLE));
-        } else {
-            if (hp > health && mc.player.fallDistance <= 5f && mode != Mode.TOTEM) {
-                switch (mode) {
-                    case GAPPLE:
-                        ItemUtil.swapToOffhandSlot(ItemUtil.getItemSlot(Items.GOLDEN_APPLE));
-                        break;
-                    case CRYSTAL:
-                        ItemUtil.swapToOffhandSlot(ItemUtil.getItemSlot(Items.END_CRYSTAL));
-                        break;
-                }
+        if (hp > health) {
+            Item heldItem = mc.player.getHeldItemMainhand().getItem();
+            if (rightClickGapple && mc.gameSettings.keyBindUseItem.isKeyDown() && (heldItem instanceof ItemSword || heldItem instanceof ItemAxe) && mc.currentScreen == null) {
+                if (mode != Mode.GAPPLE)
+                    ItemUtil.swapToOffhandSlot(getSlot(Mode.GAPPLE));
             } else {
-                ItemUtil.swapToOffhandSlot(ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING));
-                return;
+                ItemUtil.swapToOffhandSlot(getSlot(mode));
             }
+        } else {
+            ItemUtil.swapToOffhandSlot(ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING));
         }
     }
-    public enum Mode { TOTEM, GAPPLE, CRYSTAL }
+
+    private int getSlot(Mode mode) {
+        switch (mode) {
+            case CRYSTAL:
+                return ItemUtil.getItemSlot(Items.END_CRYSTAL);
+            case GAPPLE:
+                return ItemUtil.getGappleSlot(crapple);
+            default:
+                return ItemUtil.getItemSlot(Items.TOTEM_OF_UNDYING);
+        }
+    }
+
+    public enum Mode {
+        TOTEM,
+        GAPPLE,
+        CRYSTAL
+    }
+
 }
