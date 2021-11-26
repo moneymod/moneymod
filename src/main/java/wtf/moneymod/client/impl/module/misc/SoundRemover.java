01656b70e02b4a6f8d4c6c4c234fd36c5488e39f
mr-nv <31269663+mr-nv@users.noreply.github.com>
2021-11-26 21:24:10 +0300

    Create SoundRemover.java

---
+++ src/main/java/wtf/moneymod/client/impl/module/misc/SoundRemover.java
@@ -0,0 +1,32 @@
+package wtf.moneymod.client.impl.module.misc;
+
+import net.minecraft.init.SoundEvents;
+import net.minecraft.network.play.server.SPacketSoundEffect;
+import wtf.moneymod.client.api.events.PacketEvent;
+import wtf.moneymod.client.api.setting.annotatable.Value;
+import wtf.moneymod.client.impl.module.Module;
+import wtf.moneymod.eventhandler.listener.Handler;
+import wtf.moneymod.eventhandler.listener.Listener;
+
+@Module.Register( label = "SoundRemover", desc = "Mutes various sounds", cat = Module.Category.MISC )
+public class SoundRemover extends Module
+{
+    @Value( "ChorusTeleport" ) public boolean chorusteleport = true;
+    @Value( "TotemPop" ) public boolean totempop = true;
+    @Value( "Explosions" ) public boolean explosions = true;
+
+    @Handler
+    public Listener< PacketEvent.Receive > onReceivePacket = new Listener< >( PacketEvent.Receive.class, event ->
+    {
+        final SPacketSoundEffect packet = event.getPacket( );
+        if ( chorusteleport && packet.getSound( ) == SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT ) {
+            event.cancel( );
+        }
+        if ( totempop && packet.getSound( ) == SoundEvents.ITEM_TOTEM_USE ) {
+            event.cancel( );
+        }
+        if ( explosions && packet.getSound( ) == SoundEvents.ENTITY_GENERIC_EXPLODE ) {
+            event.cancel( );
+        }
+    } );
+}
