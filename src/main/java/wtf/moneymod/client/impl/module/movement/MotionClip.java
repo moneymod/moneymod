1c2da520c33bc51cac4585cce95f6dd3d1198579
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-11-27 20:11:25 +0300

    Update MotionClip.java

--- src/main/java/wtf/moneymod/client/impl/module/movement/MotionClip.java
+++ src/main/java/wtf/moneymod/client/impl/module/movement/MotionClip.java
@@ -18,83 +18,62 @@ import javax.sound.sampled.Clip;
 public class MotionClip extends Module
 {
 
-    @Value(value = "Clip Method") public Clip clip = Clip.SETPOS;
     @Value(value = "Cancel Motion") public boolean cancelMotion = false;
-    @Value(value = "Speed Mode") public Speed speed = Speed.MEDIUM;
-    @Value(value = "YSlow Updater") public boolean ySlowUpdater = false;
+    @Value(value = "Speed Mode") public Clip clip = Clip.FIRST;
+    @Value(value = "Sync Delay") @Bounds(max = 10) public int delay = 1;
     @Value(value = "Glide") public boolean glide = false;
+    @Value(value = "YUpdater") public boolean yUpdater = false;
     @Value(value = "No Clip") public boolean noClip = false;
-    @Value(value = "Reduction") public boolean reduction = false;
-    @Value(value = "Reduction XZ") @Bounds(max = 5) public float reductionXZ = 0.1f;
-    @Value(value = "Reduction Only Sneak") public boolean isReduction = false;
-
-    public enum Speed{
-        FAST, MEDIUM, SLOW, VERYSLOW;
-    }
 
     public enum Clip{
-        SETPOS, SETANGLES, OLDMETHOD,
+        FIRST,SECOND
     }
 
+    int syncdelay;
+
     public float getSpeed(){
-        switch (speed){
-            case MEDIUM:
-                return 0.05F;
-            case SLOW:
-                return 0.01F;
-            case FAST:
-                return 0.1f;
-            case VERYSLOW:
-                return 0.003f;
-        }
-        return 0.1F;
+        return 0.008F;
     }
 
     @Override
     public void onTick() {
+        syncdelay++;
         if (nullCheck()) return;
         double[] s = EntityUtil.forward(getSpeed());
         mc.player.noClip = this.noClip;
         if (glide) mc.player.motionY = 0;
         double yaw = mc.player.rotationYaw * 0.017453292;
         double power = getSpeed();
+
+
         if (cancelMotion){
             mc.player.motionZ = 0;
             mc.player.motionX = 0;
         }
 
         if (mc.gameSettings.keyBindSneak.isKeyDown()) {
-            if (clip == Clip.OLDMETHOD){
-                mc.player.setPosition(mc.player.posX, mc.player.posY, mc.player.posZ);
-                mc.player.setPosition(mc.player.posX + s[0], mc.player.posY + (ySlowUpdater ? 0.001 : 0), mc.player.posZ + s[1]);
+            if (clip == Clip.SECOND) {
+                if (mc.player.collidedHorizontally) {
+                    mc.player.setLocationAndAngles(mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.rotationYaw, mc.player.rotationPitch);
+                    mc.player.setLocationAndAngles(mc.player.posX - Math.sin(yaw) * power, mc.player.posY, mc.player.posZ + Math.cos(yaw) * power, mc.player.rotationYaw, mc.player.rotationPitch);
+                } else {
+                    if (!EntityUtil.INSTANCE.isMoving(mc.player)) {
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.onGround));
+                        mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY - 1, mc.player.posZ, mc.player.onGround));
+                    }
+                }
             }
-            if (clip == Clip.SETPOS){
-                mc.player.setPosition(mc.player.posX, mc.player.posY, mc.player.posZ);
-                mc.player.setPosition(mc.player.posX - Math.sin(yaw) * power, mc.player.posY, mc.player.posZ + Math.cos(yaw) * power);
-            }
-            if (clip == Clip.SETANGLES){
-                mc.player.setLocationAndAngles(mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.rotationYaw, mc.player.rotationPitch);
-                mc.player.setLocationAndAngles(mc.player.posX - Math.sin(yaw) * power, mc.player.posY, mc.player.posZ + Math.cos(yaw) * power, mc.player.rotationYaw, mc.player.rotationPitch);
 
+            if (clip == Clip.FIRST) {
+                mc.player.setPosition(mc.player.posX, mc.player.posY, mc.player.posZ);
+                mc.player.setPosition(mc.player.posX + s[0], mc.player.posY + (yUpdater ? 0.01 : 0), mc.player.posZ + s[1]);
             }
         }
     }
 
-    @Handler
-    public Listener<MoveEvent> onMove = new Listener<>(MoveEvent.class, e -> {
-        if (nullCheck()) return;
-        if (isReduction) {
-            if (mc.gameSettings.keyBindSneak.isKeyDown()) {
-                if (reduction) {
-                    e.motionX = e.motionX * reductionXZ / 10.0;
-                    e.motionZ = e.motionZ * reductionXZ / 10.0;
-                }
-            }
-        } else {
-            if (reduction) {
-                e.motionX = e.motionX * reductionXZ / 10.0;
-                e.motionZ = e.motionZ * reductionXZ / 10.0;
-            }
-        }
-    });
+    public void sendPackets(double q, double w, double r) {
+        mc.getConnection().sendPacket(new CPacketPlayer.Position(q, w, r, mc.player.onGround));
+        mc.getConnection().sendPacket(new CPacketPlayer.Position(0,767, 0, mc.player.onGround));
+    }
+
 }
