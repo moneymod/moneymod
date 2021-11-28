6bb80a025ce015c191f5715e5dcbca3269772bc1
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-11-28 22:04:45 +0300

    топчик ExpTweaks

--- src/main/java/wtf/moneymod/client/impl/module/misc/ExpTweaks.java
+++ src/main/java/wtf/moneymod/client/impl/module/misc/ExpTweaks.java
@@ -3,6 +3,7 @@ package wtf.moneymod.client.impl.module.misc;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemExpBottle;
 import net.minecraft.util.EnumHand;
+import org.lwjgl.input.Mouse;
 import wtf.moneymod.client.api.setting.annotatable.Value;
 import wtf.moneymod.client.impl.module.Module;
 import wtf.moneymod.client.impl.utility.impl.player.ItemUtil;
@@ -11,19 +12,25 @@ import wtf.moneymod.client.mixin.mixins.ducks.IMinecraft;
 @Module.Register( label = "ExpTweaks", cat = Module.Category.MISC)
 public class ExpTweaks extends Module {
 
-    @Value(value = "Silent") public boolean silent = false;
+    @Value(value = "Mode") public Mode mode = Mode.MIDDLE;
+
+    public enum Mode{
+        TOGGLE, MIDDLE
+    }
 
     @Override
     public void onTick() {
-        if (mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE && !silent) {
-            ((IMinecraft) mc).setRightClickDelayTimer(0);
+
+        int old = mc.player.inventory.currentItem;
+        int xp = ItemUtil.findItem(ItemExpBottle.class);
+        if (xp == -1) return;
+
+        if (mode == Mode.MIDDLE){
+            if (!Mouse.isButtonDown(2)) return;
         }
-        if (silent) {
-            int old = mc.player.inventory.currentItem;  int xp = ItemUtil.findItem(ItemExpBottle.class);
-            if (xp == -1) return;
-            ItemUtil.swapToHotbarSlot(xp, false);
-            mc.playerController.processRightClick(mc.player, mc.world, EnumHand.MAIN_HAND);
-            ItemUtil.swapToHotbarSlot(old, false);
-        }
+
+        ItemUtil.swapToHotbarSlot(xp, false);
+        mc.playerController.processRightClick(mc.player, mc.world, EnumHand.MAIN_HAND);
+        ItemUtil.swapToHotbarSlot(old, false);
     }
 }
