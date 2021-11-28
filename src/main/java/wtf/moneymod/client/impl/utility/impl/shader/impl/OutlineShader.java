a6ea9c9b8144764f3a6e5d69b63fe5e34cc322c6
mr-nv <31269663+mr-nv@users.noreply.github.com>
2021-11-28 12:53:35 +0300

    outline shader esp

--- src/main/java/wtf/moneymod/client/impl/utility/impl/shader/impl/OutlineShader.java
+++ src/main/java/wtf/moneymod/client/impl/utility/impl/shader/impl/OutlineShader.java
@@ -9,6 +9,17 @@ public class OutlineShader extends FramebufferShader {
         super(fragmentShader);
     }
 
+    public float rainbowSpeed = 0.5f;
+    public float rainbowStrength = 0.25f;
+    public float customSaturation = 0.5f;
+
+    public void setCustomValues( float rainbowSpeed, float rainbowStrength, float saturation )
+    {
+        this.rainbowSpeed = rainbowSpeed;
+        this.rainbowStrength = rainbowStrength;
+        this.customSaturation = saturation;
+    }
+
     @Override public void setupUniforms() {
         this.setupUniform("texture");
         this.setupUniform("texelSize");
@@ -24,11 +35,14 @@ public class OutlineShader extends FramebufferShader {
         GL20.glUniform2f(this.getUniform("texelSize"), 1.0f / this.mc.displayWidth * (this.radius * this.quality), 1.0f / this.mc.displayHeight * (this.radius * this.quality));
         GL20.glUniform4f(this.getUniform("color"), this.red, this.green, this.blue, this.alpha);
         GL20.glUniform1f(this.getUniform("radius"), this.radius);
-        GL20.glUniform2f(this.getUniform("rainbowStrength"), x, y);
-        GL20.glUniform1f(this.getUniform("rainbowSpeed"), speed);
-        GL20.glUniform1f(this.getUniform("saturation"), saturation);
+
+        float strength = 1.0f / -( 1000.0f * rainbowStrength );
+
+        GL20.glUniform2f(this.getUniform("rainbowStrength"), strength, strength);
+        GL20.glUniform1f(this.getUniform("rainbowSpeed"), rainbowSpeed);
+        GL20.glUniform1f(this.getUniform("saturation"), customSaturation);
     }
 
-    public static final FramebufferShader INSTANCE = new OutlineShader("outline.frag");
+    public static final OutlineShader INSTANCE = new OutlineShader("outline.frag");
 
 }
