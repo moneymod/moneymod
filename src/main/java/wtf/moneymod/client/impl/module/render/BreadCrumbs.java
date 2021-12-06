12542b99c1bc49e23b7c860363cdb6e8fc534495
cattyngmd <cvenby@gmail.com>
2021-12-06 16:54:26 +0300

    q-okoasfkop[gksipgkmpsmgpksmphg

--- src/main/java/wtf/moneymod/client/impl/module/render/BreadCrumbs.java
+++ src/main/java/wtf/moneymod/client/impl/module/render/BreadCrumbs.java
@@ -1,38 +1,39 @@
 package wtf.moneymod.client.impl.module.render;
 
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.Vec3d;
 import org.lwjgl.opengl.GL11;
+import wtf.moneymod.client.Main;
 import wtf.moneymod.client.api.setting.annotatable.Bounds;
 import wtf.moneymod.client.api.setting.annotatable.Value;
 import wtf.moneymod.client.impl.module.Module;
-import wtf.moneymod.client.impl.module.misc.ServerFlag;
-import wtf.moneymod.client.impl.utility.impl.render.CrumbsUtil;
+import wtf.moneymod.client.impl.utility.impl.misc.Timer;
 import wtf.moneymod.client.impl.utility.impl.render.JColor;
+import wtf.moneymod.client.impl.utility.impl.world.EntityUtil;
 
 import java.util.ArrayList;
 
-@Module.Register( label = "BreadCrumbs", cat = Module.Category.RENDER)
+@Module.Register( label = "BreadCrumbs", cat = Module.Category.RENDER )
 public class BreadCrumbs extends Module {
 
-    ArrayList<CrumbsUtil> bcs = new ArrayList<CrumbsUtil>();
+    ArrayList<BreadCrumb> bcs = new ArrayList<>();
 
-    @Value(value = "Mode") public Mode mode = Mode.DEFAULT;
-    @Value(value = "Line Widht") @Bounds(min = 0.1F, max = 5) public float lineWidht = 1;
-    @Value(value = "Tick Clear") @Bounds(min = 1, max = 45) public int tickClear = 25;
+    @Value( value = "Mode" ) public Mode mode = Mode.DEFAULT;
+    @Value( value = "Line Widht" ) @Bounds( min = 0.1F, max = 5 ) public float lineWidht = 1;
+    @Value( value = "Tick Clear" ) @Bounds( min = 1, max = 45 ) public int tickClear = 25;
+    @Value("Fade Speed") @Bounds(min = 0.1f, max = 2f) public float fadeSpeed = 0.4f;
     @Value( "Color" ) public JColor color = new JColor(255, 255, 255, false);
 
-    public enum Mode{
-        DEFAULT, PARTICLE
+    public enum Mode {
+        DEFAULT,
+        PARTICLE
     }
 
     @Override
     public void onTick() {
         if (nullCheck()) return;
-        if (mode == Mode.DEFAULT) bcs.add(new CrumbsUtil(mc.player.getPositionVector()));
-        if (mode == Mode.PARTICLE) mc.world.spawnParticle(EnumParticleTypes.DRIP_WATER, mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.motionX, mc.player.motionY, mc.player.motionZ, 2);
+        if (mode == Mode.PARTICLE)
+            mc.world.spawnParticle(EnumParticleTypes.DRIP_WATER, mc.player.posX, mc.player.posY, mc.player.posZ, mc.player.motionX, mc.player.motionY, mc.player.motionZ, 2);
     }
 
 
@@ -46,14 +47,16 @@ public class BreadCrumbs extends Module {
     }
 
     @Override
-    public void onToggle(){
+    public void onToggle() {
         bcs.clear();
     }
 
     @Override
     public void onRender3D(float partialTicks) {
+        if (mode == Mode.DEFAULT && EntityUtil.INSTANCE.isMoving(mc.player))
+            bcs.add(new BreadCrumb(new Vec3d(mc.player.posX, mc.player.posY, mc.player.posZ)));
         if (mode == Mode.DEFAULT) {
-            int time = tickClear * 500;
+            int time = tickClear * 50;
             GL11.glPushMatrix();
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glEnable(GL11.GL_LINE_SMOOTH);
@@ -61,14 +64,14 @@ public class BreadCrumbs extends Module {
             GL11.glBlendFunc(770, 771);
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glLineWidth(lineWidht);
-            GL11.glColor4f(color.getColor().getRed() / 255.0F, color.getColor().getGreen() / 255.0f, color.getColor().getBlue() / 255.0f, color.getColor().getAlpha() / 255.0f);
             GL11.glBegin(3);
             for (int i = 0; i < bcs.size(); i++) {
-                CrumbsUtil bc = bcs.get(i);
-                if (bc.getTimer().passed(time)) {
-                    bcs.remove(bc);
+                BreadCrumb crumb = bcs.get(i);
+                if (crumb.getTimer().passed(time)) {
+                    crumb.update(bcs);
                 }
-                putVertex3d(getRenderPos(bc.getVector().x, bc.getVector().y + 0.3, bc.getVector().z));
+                GL11.glColor4f(color.getColor().getRed() / 255.0F, color.getColor().getGreen() / 255.0f, color.getColor().getBlue() / 255.0f, ( float ) (crumb.getAlpha() / 255f));
+                putVertex3d(getRenderPos(crumb.getVector().x, crumb.getVector().y + 0.3, crumb.getVector().z));
             }
             GL11.glEnd();
             GL11.glDisable(GL11.GL_BLEND);
@@ -78,4 +81,40 @@ public class BreadCrumbs extends Module {
             GL11.glPopMatrix();
         }
     }
+
+    public class BreadCrumb {
+
+        Vec3d vector;
+        Timer timer;
+        double alpha;
+
+        public BreadCrumb(Vec3d vector) {
+            timer = new Timer();
+            timer.reset();
+            this.vector = vector;
+            this.alpha = color.getColor().getAlpha();
+        }
+
+        public Timer getTimer() {
+            return timer;
+        }
+
+        public Vec3d getVector() {
+            return vector;
+        }
+
+        public double getAlpha() {
+            return alpha;
+        }
+
+        public void update(ArrayList arrayList) {
+            if (alpha <= 0) {
+                alpha = 0;
+                arrayList.remove(this);
+            }
+            this.alpha -= color.getColor().getAlpha() / fadeSpeed * Main.getMain().getFpsManagement().getFrametime();
+        }
+
+    }
+
 }
