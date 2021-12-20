a795601fbb45c61161e19bee6db0c0f85a07a1f3
kisman <81470009+kisman2000@users.noreply.github.com>
2021-12-20 23:52:01 +0300

    Delete NoSlowSneak.java

--- src/main/java/wtf/moneymod/client/impl/module/movement/NoSlowSneak.java
+++
@@ -1,7 +0,0 @@
-package wtf.moneymod.client.impl.module.movement;
-
-import wtf.moneymod.client.impl.module.Module;
-
-@Module.Register(label = "NoSlowSneak", cat = Module.Category.MOVEMENT)
-public class NoSlowSneak extends Module {
-}
